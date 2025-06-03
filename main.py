247d48d625332c1fa1f006f887e48e47747a1fa3
DESKTOP-7A91CF5\Kin <ken.kin@msa.hinet.net>
2025-06-03 23:01:43 +0800

    8080

--- main.py
+++ main.py
@@ -77,4 +77,4 @@ async def request_handler(request: Request):
 if __name__ == '__main__':
     import uvicorn
 
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="info", reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8080, log_level="info", reload=True)
