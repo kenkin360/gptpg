53dc17f1e7f0c5d428a62718e34c5de2f2bd068c
DESKTOP-7A91CF5\Kin <ken.kin@msa.hinet.net>
2025-06-03 22:39:06 +0800

    https://chatgpt.com/

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask
 from requests import get
 
 app = Flask(__name__)
-SITE_NAME = 'https://google.com/'
+SITE_NAME = 'https://chatgpt.com/'
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
