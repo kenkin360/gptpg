e005283d272d416773292de0768285b76a670f0c
DESKTOP-7A91CF5\Kin <ken.kin@msa.hinet.net>
2025-06-04 03:55:46 +0800

    8080

--- main.py
+++ main.py
@@ -118,5 +118,5 @@ def form_proxy():
     return redirect(url_for('browser') + '?url=' + request.args.get('url'))
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(host="0.0.0.0", port=port)
